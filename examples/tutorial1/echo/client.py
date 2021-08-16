1d86dce52190e428a37e62bb714df18b987ae2b5
Amaranese <alejandro.maranes@asendia.com>
2021-08-16 23:47:39 +0200

    Create client.py

---
+++ examples/tutorial1/echo/client.py
@@ -0,0 +1,22 @@
+"""This is the client code for the Asphalt echo server tutorial."""
+import sys
+from asyncio import open_connection
+
+from asphalt.core import CLIApplicationComponent, run_application
+
+
+class ClientComponent(CLIApplicationComponent):
+    def __init__(self, message: str):
+        super().__init__()
+        self.message = message
+
+    async def run(self, ctx):
+        reader, writer = await open_connection('localhost', 64100)
+        writer.write(self.message.encode() + b'\n')
+        response = await reader.readline()
+        writer.close()
+        print('Server responded:', response.decode().rstrip())
+
+if __name__ == '__main__':
+    component = ClientComponent(sys.argv[1])
+    run_application(component)
