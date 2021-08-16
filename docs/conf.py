fb3969bb052d4e874f10577f2ae06fb256118c4e
Amaranese <alejandro.maranes@asendia.com>
2021-08-16 23:48:40 +0200

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import pkg_resources
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinx_autodoc_typehints',
+    'sphinxcontrib.asyncio'
+]
+
+templates_path = ['_templates']
+source_suffix = '.rst'
+master_doc = 'index'
+project = 'The Asphalt Framework (core)'
+author = 'Alex Grönholm'
+copyright = '2015, ' + author
+
+v = pkg_resources.get_distribution('asphalt').parsed_version
+version = v.base_version
+release = v.public
+
+language = None
+
+exclude_patterns = ['_build']
+pygments_style = 'sphinx'
+highlight_language = 'python3'
+todo_include_todos = False
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+htmlhelp_basename = 'asphaltdoc'
+
+intersphinx_mapping = {'python': ('http://docs.python.org/3/', None)}
